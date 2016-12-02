3d1fe32ef7e42175509fa89a07b63206b12b294a
Kaylleur <dev@thomas-brunet.fr>
2016-12-02 17:16:12 +0100

    Doc

---
+++ DirtySkunk/DirtySkunk.Droid/obj/Debug/android/src/md53f1e24a08b3a55b9b6a547906d6dcb95/SplashScreen.java
@@ -0,0 +1,38 @@
+package md53f1e24a08b3a55b9b6a547906d6dcb95;
+
+
+public class SplashScreen
+	extends mvvmcross.droid.views.MvxSplashScreenActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"";
+		mono.android.Runtime.register ("DirtySkunk.Droid.SplashScreen, DirtySkunk.Droid, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", SplashScreen.class, __md_methods);
+	}
+
+
+	public SplashScreen () throws java.lang.Throwable
+	{
+		super ();
+		if (getClass () == SplashScreen.class)
+			mono.android.TypeManager.Activate ("DirtySkunk.Droid.SplashScreen, DirtySkunk.Droid, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "", this, new java.lang.Object[] {  });
+	}
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
